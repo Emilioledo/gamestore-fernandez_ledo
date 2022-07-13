a0c69bd9eeb3070e69db1d434689aa8e8bd8003d
emilioledo <fernandezledoemilio@gmail.com>
2022-07-13 15:42:07 -0300

    navbar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <div className="topnav">
+      <h3>GAME STORE</h3>
+      <div className="topnav-right">
+        <a href="#news">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    </div>
+  )
+}
