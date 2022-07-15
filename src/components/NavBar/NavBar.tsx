6f81316cca9de26eb2a6ec2b78e364ef0b3eec64
emilioledo <fernandezledoemilio@gmail.com>
2022-07-15 16:07:01 -0300

    cart widget

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,12 @@
 import './NavBar.css'
+import CartWidget from '../Widget/CartWidget'
 
 export default function NavBar() {
   return (
     <div className="topnav">
       <h3>GAME STORE</h3>
       <div className="topnav-right">
-        <a href="#news">Home</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <CartWidget />
       </div>
     </div>
   )
---
+++ src/components/Widget/CartWidget.tsx
@@ -0,0 +1,10 @@
+import './Widget.css'
+import { FaShoppingCart } from 'react-icons/fa'
+
+export default function CartWidget() {
+  return (
+    <button>
+      <FaShoppingCart color="white" />
+    </button>
+  )
+}
