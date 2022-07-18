68e3e48e1035e28149378e324286f4ede0450ce0
emilioledo <fernandezledoemilio@gmail.com>
2022-07-18 11:20:06 -0300

    widget styles

--- src/components/Widget/CartWidget.tsx
+++ src/components/Widget/CartWidget.tsx
@@ -3,8 +3,9 @@ import { FaShoppingCart } from 'react-icons/fa'
 
 export default function CartWidget() {
   return (
-      <button className='button'>
-        <FaShoppingCart color="white" />
-      </button>
+    <button className="button">
+      <FaShoppingCart color="white" className="icon"/>
+      <span>Cart</span>
+    </button>
   )
 }
